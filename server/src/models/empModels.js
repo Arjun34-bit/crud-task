772da0868e669eeee83779583f55d8440bafa7a2
Arjun Madhu Devendra <ARJUNDEVENDRA34@GMAIL.COM>
2025-08-18 21:19:00 +0530

    Fix error message in updateEmployeeService for accuracy

--- server/src/models/empModels.js
+++ server/src/models/empModels.js
@@ -57,7 +57,7 @@ export const updateEmployeService = async (id, data) => {
     );
     return result.rows[0];
   } catch (error) {
-    console.error("Error deleting employees:", error);
+    console.error("Error deleting employee:", error);
     throw error;
   }
 };
