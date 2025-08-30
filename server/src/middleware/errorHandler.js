dd1ec41b0e8f032a549ccf55d4a0a571fe185644
Arjun Madhu Devendra <ARJUNDEVENDRA34@GMAIL.COM>
2025-08-30 22:57:50 +0530

    fix: modified errorHandler

--- server/src/middleware/errorHandler.js
+++ server/src/middleware/errorHandler.js
@@ -1,10 +1,9 @@
 const errorHandling = (err, req, res, next) => {
   console.log(err.stack);
-  res.status(500),
+  res.status(err.status || 500),
     json({
-      status: 500,
-      message: "Something went wrong",
-      error: err.message,
+      status: err.status || 500,
+      error: err.message || "Internal Server Error",
     });
 };
 
